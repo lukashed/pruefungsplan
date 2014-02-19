9d9d577e92ccd12705541c521d653c3b1903db51
Lukas Klein <lukas@productgang.com>
2014-02-19 10:59:20 +0100

    form validation

--- pruefungsplan/notifier/forms.py
+++ pruefungsplan/notifier/forms.py
@@ -10,6 +10,21 @@ class SignUpForm(forms.Form):
         queryset=Pruefungsplan.objects.filter(is_available=False), initial=0,
     )
     email = forms.EmailField()
+
+    def clean_email(self):
+        pruefungsplan = self.cleaned_data['pruefungsplan']
+        email = self.cleaned_data.get('email', '')
+        if Notification.objects.filter(
+            email=email,
+            pruefungsplan=pruefungsplan
+        ).exists():
+            raise forms.ValidationError(
+                'Diese Email Adresse bekommt bereits eine Benachrichtigung \
+                fuer den Pruefungsplan \
+                %s!' % pruefungsplan.name
+            )
+        return email
+
     phone_number = forms.CharField(
         required=False,
         label='Handynummer',
@@ -17,6 +32,20 @@ class SignUpForm(forms.Form):
         Format: z.B. +4915207921346',
         initial='+49',
     )
+
+    def clean_phone_number(self):
+        pruefungsplan = self.cleaned_data['pruefungsplan']
+        number = self.cleaned_data.get('phone_number', '')
+        if Notification.objects.filter(
+            sms=number,
+            pruefungsplan=pruefungsplan
+        ).exists():
+            raise forms.ValidationError(
+                'Diese Handynummer bekommt bereits eine Benachrichtigung \
+                fuer den Pruefungsplan \
+                %s!' % pruefungsplan.name
+            )
+        return number
 
     helper = FormHelper()
     helper.form_action = '/'
