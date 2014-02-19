31babcba745f530c8cc5733f14b1276f44966a10
Lukas Klein <lukas@productgang.com>
2014-02-19 11:13:06 +0100

    static pages

--- pruefungsplan/config/urls.py
+++ pruefungsplan/config/urls.py
@@ -22,6 +22,9 @@ urlpatterns = patterns('',
     url(r'^about/$',
         TemplateView.as_view(template_name='pages/about.html'),
         name="about"),
+    url(r'^impressum/$',
+        TemplateView.as_view(template_name='pages/imprint.html'),
+        name="imprint"),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
